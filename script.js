317688249a6f22d3a9d66eea193d545fbe69d608
bob24170 <58536544+bob24170@users.noreply.github.com>
2023-06-27 06:17:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 const pictureUrls = [
  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgGlrzl7AIu-X0cabj8fgX4XD31Qh5l0bd7g&usqp=CAU',
 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6lpAlqJi8rGvsdBsiDCcoiXYjBPGzh_fqA&usqp=CAU',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1mf3i4nYZ2EJVTnxYf9n3U8S-PuUjdeVemB0IUyaeeUTcGNqDmLqdgIZ4p0ybHqoxWfs&usqp=CAU'
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1mf3i4nYZ2EJVTnxYf9n3U8S-PuUjdeVemB0IUyaeeUTcGNqDmLqdgIZ4p0ybHqoxWfs&usqp=CAU',
+ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh9ZZR9xAjX7jg3XxctmWNgztGYwLzmh3nlQ&usqp=CAU',
+ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBA4J_9Zq56wjS1oS5X246fYsBOPft39wgVg&usqp=CAU',
+ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1mf3i4nYZ2EJVTnxYf9n3U8S-PuUjdeVemB0IUyaeeUTcGNqDmLqdgIZ4p0ybHqoxWfs&usqp=CAU'
   // Add more picture URLs as needed
 ];
 
