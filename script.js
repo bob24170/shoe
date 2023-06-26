ff56fd8b467f8ba366a78afdef1077fec0637143
bob24170 <58536544+bob24170@users.noreply.github.com>
2023-06-27 06:20:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // Define an array of picture URLs
 const pictureUrls = [
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgGlrzl7AIu-X0cabj8fgX4XD31Qh5l0bd7g&usqp=CAU',
-'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv6lpAlqJi8rGvsdBsiDCcoiXYjBPGzh_fqA&usqp=CAU',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1mf3i4nYZ2EJVTnxYf9n3U8S-PuUjdeVemB0IUyaeeUTcGNqDmLqdgIZ4p0ybHqoxWfs&usqp=CAU',
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh9ZZR9xAjX7jg3XxctmWNgztGYwLzmh3nlQ&usqp=CAU',
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBA4J_9Zq56wjS1oS5X246fYsBOPft39wgVg&usqp=CAU',
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1mf3i4nYZ2EJVTnxYf9n3U8S-PuUjdeVemB0IUyaeeUTcGNqDmLqdgIZ4p0ybHqoxWfs&usqp=CAU'
+  'https://example.com/image1.jpg',
+  'https://example.com/image2.jpg',
+  'https://example.com/image3.jpg',
   // Add more picture URLs as needed
 ];
 
